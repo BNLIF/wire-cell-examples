fbec08a9b845b9ae76ddbc4e3d1453138ce3db5e
Brett Viren <bv@bnl.gov>
2015-03-31 12:06:22 -0400

    React to degeneracy changes, add timing measurements.

--- apps/wire-cell-example-loop.cxx
+++ apps/wire-cell-example-loop.cxx
@@ -5,6 +5,7 @@
 #include "TFile.h"
 #include "TTree.h"
 
+#include <ctime>
 #include <fstream>
 #include <iostream>
 using namespace std;
@@ -25,12 +26,16 @@ int main(int argc, char* argv[])
     TTree* tree = dynamic_cast<TTree*>(tfile->Get("/Event/Sim"));
     WireCellSst::FrameDataSource fds(*tree);
     
-    WireCell::SliceDataSource sds(fds);
+    WireCell::SliceDataSource sds(fds, gds);
     
     // Loop over frames (aka "events")
     size_t nframes = fds.size();
     cout << "FDS: " << nframes << " frames" << endl;
+
+    time_t start_time = time(0);
     for (size_t iframe = 0; iframe < nframes; ++iframe) {
+
+	cout << "FDS: jumping to frame #" << iframe << endl;
 
 	int iframe_got = fds.jump(iframe);
 	if (iframe_got < 0) {
@@ -40,9 +45,17 @@ int main(int argc, char* argv[])
 
 	// loop over slices of the frame 
 
+	time_t start_frame_time = time(0);
+
 	size_t nslices = sds.size();
 	cout << "SDS: " << nslices << " slices in frame " << iframe_got << endl;
 	for (size_t islice = 0; islice < nslices; ++islice) {
+	    if (islice%1000 == 1) {
+		time_t now = time(0);
+		cerr << "slice #" << islice
+		     << " time elapsed in frame: " << now-start_frame_time
+		     << endl;
+	    }
 	    int islice_got = sds.jump(islice);
 	    if (islice_got < 0) {
 		cerr << "Failed to get slice " << islice << " from frame " << iframe << endl;
@@ -50,9 +63,13 @@ int main(int argc, char* argv[])
 	    }
 
 	    const WireCell::Slice& slice = sds.get();
-
-	    cout << "SDS:\t tbin=" << slice.tbin << " #bins:" << slice.charge.size() << endl;
 	}
+
+	time_t now = time(0);
+	cerr << "frame #" << iframe
+	     << " time elapsed for frame: " << now-start_frame_time
+	     << " total time: " << now-start_time
+	     << endl;
     }
 
     return 0;
